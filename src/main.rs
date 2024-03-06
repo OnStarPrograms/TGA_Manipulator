b778be16e06693fd37c5cd6737dfe68d4d9418e1
OnStarPrograms <on@starprograms.dev>
2024-03-06 15:30:15 -0500

    added -C

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
 use std::fs;
+// use std::io;
 // use std::path::Path;
 
 fn main() -> std::io::Result<()>{
@@ -11,8 +12,10 @@ fn main() -> std::io::Result<()>{
     // println!("Successfully changed working directory to {}!", root.display());
     // assert!(env::set_current_dir(&root).is_ok());
     // Fuck dude this was difficult just to read a txt file but I finally did it
+    let mut i = 0;
     for element in args.iter() 
     {
+        // pulls the poem and the car.tga and prints them
         if element == "-R"
         {
             // dont add a "/" to the front of the path argument
@@ -36,6 +39,24 @@ fn main() -> std::io::Result<()>{
             let path = env::current_dir()?;
             print!("{}", path.display());
         }
+        // plan to Use to call a change function format CLI {cargo run -- -C input/file1 input/file2 manipulator}
+        // need to build this pls
+        // currently can only do (cargo run -- -C input/file1)
+        if element == "-C"
+        {
+            let path: String = String::from(&args[i+1]);
+            // let mut path: String = String::new();
+            // io::stdin().read_line(&mut path).expect("failed to read line.");  
+            let bytes = fs::read(path).unwrap(); 
+            let mut vec: Vec<u8> = Vec::new();
+            for byte_pair in bytes.chunks_exact(1)
+            {
+                let short = u8::from_le_bytes([byte_pair[0]]);
+                vec.push(short);
+                print!("{:x} ", short);
+            }
+        }
+        i += 1;
     }
 
     // Everything is a-OK
